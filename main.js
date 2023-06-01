d53753c84c3a4dcffa6948782d1e83451aad5f9a
sebas <sebas28ac@gmail.com>
2023-06-01 19:45:00 +0200

    Game v2

--- main.js
+++ main.js
@@ -1,50 +1,64 @@
-//Mejoras:
-//1. Cambiar el array por un objeto para facilitar la lectura del código. P.e.: {Enunciado: 'Alemania', Respuesta1: 'Viena', Respuesta2: 'Berlin', Respuesta3: 'Bruselas', Respuesta4: 'Nicosia', Imagen: 'germany', Correcta: 'Berlin'}
-//2. Añadir más contienentes y separarlos en preguntas
-//3. Guardar puntuación máxima en la pantalla de inicio (con localstorage)
-
 // Declarar variables
-// Ordenadas siguiendo el orden en el que aparecen
 let numeroPuntos;
 let numeroPregunta;
-let ordenPreguntas = [0, 1, 2, 3, 4, 5];
-let ordenRespuestas = [1, 2, 3, 4];
 let mostrarNumeroPregunta = document.getElementById('mostrarNumeroPregunta');
 let enunciado = document.getElementById('enunciado');
 let respuesta = document.getElementById('botonRespuesta');
 let puntos = document.getElementById('puntos');
-let listaPreguntas = [
-    ['ALEMANIA', 'Viena', 'Berlín', 'Bruselas', 'Nicosia', 'germany', 'Berlín'],
-    ['ANDORRA', 'Tallin', 'Minsk', 'Andorra la Vieja', 'Copenhage', 'andora', 'Andorra la Vieja'],
-    ['BULGARIA', 'Helsinki', 'Zagreb', 'Sofía', 'París', 'bulgaria', 'Sofía'],
-    ['GRECIA', 'Atenas', 'Copenhague', 'Tirana', 'Sarajevo', 'greece', 'Atenas'],
-    ['AUSTRIA', 'Madrid', 'Bratislava', 'Bruselas', 'Viena', 'austria', 'Viena'],
-    ['DINAMARCA', 'Bratislava', 'Copenhague', 'Dublín', 'Luxemburgo', 'denmark', 'Copenhague'],
-];
-// let respuestaCorrecta = [2, 3, 3, 1, 4, 2]
+let puntuacionFinal = document.getElementById('puntuacionFinal');
+let puntuacionMaxima = document.getElementById('puntuacionMaxima');
 let sonidoAcierto = new Audio('./sounds/acierto.mp3');
 let sonidoFallo = new Audio('./sounds/fallo.mp3');
-let puntuacionFinal = document.getElementById('puntuacionFinal');
-
+let datos = {
+    europa: [
+        { enunciado: 'ALEMANIA', respuesta: ['Berlín', 'Viena', 'Bruselas', 'Nicosia'], imagen: 'germany', correcta: 'Berlín' },
+        { enunciado: 'ANDORRA', respuesta: ['Andorra la Vieja', 'Tallin', 'Minsk', 'Copenhague'], imagen: 'andora', correcta: 'Andorra la Vieja' },
+        { enunciado: 'BULGARIA', respuesta: ['Sofía', 'Helsinki', 'Zagreb', 'París'], imagen: 'bulgaria', correcta: 'Sofía' },
+        { enunciado: 'GRECIA', respuesta: ['Atenas', 'Copenhague', 'Tirana', 'Sarajevo'], imagen: 'greece', correcta: 'Atenas' },
+        { enunciado: 'AUSTRIA', respuesta: ['Viena', 'Madrid', 'Bratislava', 'Bruselas'], imagen: 'austria', correcta: 'Viena' },
+        { enunciado: 'DINAMARCA', respuesta: ['Copenhague', 'Bratislava', 'Dublín', 'Luxemburgo'], imagen: 'denmark', correcta: 'Copenhague' }
+    ],
+    america: [
+        { enunciado: 'ESTADOS UNIDOS', respuesta: ['Washington D. C.', 'Bogotá', 'Quito', 'Kingston'], imagen: 'usa', correcta: 'Washington D. C.' },
+        { enunciado: 'CANADÁ', respuesta: ['Ottawa', 'Ciudad de México', 'Santiago', 'La Habana'], imagen: 'canada', correcta: 'Ottawa' },
+        { enunciado: 'MÉXICO', respuesta: ['Ciudad de México', 'Brasilia', 'Ottawa', 'Buenos Aires'], imagen: 'mexico', correcta: 'Ciudad de México' },
+        { enunciado: 'COLOMBIA', respuesta: ['Bogotá', 'Santiago', 'Quito', 'Lima'], imagen: 'colombia', correcta: 'Bogotá' },
+        { enunciado: 'VENEZUELA', respuesta: ['Caracas', 'Brasilia', 'Lima', 'La Habana'], imagen: 'venezuela', correcta: 'Caracas' },
+        { enunciado: 'BRASIL', respuesta: ['Brasilia', 'Santiago', 'Lima', 'Buenos Aires'], imagen: 'brazil', correcta: 'Brasilia' }
+    ],
+    asia: [
+        { enunciado: 'CHINA', respuesta: ['Pekín', 'Shanghai', 'Tianjin', 'Hong Kong'], imagen: 'china', correcta: 'Pekín' },
+        { enunciado: 'JAPÓN', respuesta: ['Tokio', 'Seúl', 'Nagoya', 'Osaka'], imagen: 'japan', correcta: 'Tokio' },
+        { enunciado: 'INDIA', respuesta: ['Nueva Delhi', 'Seúl', 'Daca', 'Moscú'], imagen: 'india', correcta: 'Nueva Delhi' },
+        { enunciado: 'IRAK', respuesta: ['Teherán', 'Bagdad', 'Jerusalén', 'Tripoli'], imagen: 'iraq', correcta: 'Teherán' },
+        { enunciado: 'ARABIA SAUDITA', respuesta: ['Riad', 'Manila', 'Ankara', 'Bagdad'], imagen: 'saudi-arabia', correcta: 'Riad' },
+        { enunciado: 'CHIPRE', respuesta: ['Nicosia', 'Ereván', 'Kabul', 'Yakarta'], imagen: 'cyprus', correcta: 'Nicosia' }
+    ],
+    oceania: [
+        { enunciado: 'AUSTRALIA', respuesta: ['Camberra', 'Yaren', 'Majuro', 'Brisbane'], imagen: 'australia', correcta: 'Camberra' },
+        { enunciado: 'NUEVA ZELANDA', respuesta: ['Wellington', 'Sydney', 'Auckland', 'Funafuti'], imagen: 'new-zealand', correcta: 'Wellington' },
+        { enunciado: 'VANUATU', respuesta: ['Port Vila', 'Palikir', 'Tarawa Sur', 'Nukualofa'], imagen: 'vanuatu', correcta: 'Port Vila' },
+        { enunciado: 'ISLAS SALOMÓN', respuesta: ['Honiara', 'Ngerulmud', 'Majuro', 'Port Vila'], imagen: 'tonga', correcta: 'Honiara' },
+        { enunciado: 'FIJI', respuesta: ['Suva', 'Apia', 'Nukualofa', 'Ngerulmud'], imagen: 'fiji', correcta: 'Suva' },
+        { enunciado: 'PAPÚA NUEVA GUINEA', respuesta: ['Port Moresby', 'Majuro', 'Nukualofa', 'Suva'], imagen: 'papua-new-guinea', correcta: 'Port Moresby' }
+    ]
+};
 
 // Declarar funciones
-// Ordenadas siguiendo el orden en el que aparecen
 function inicio() {
     numeroPregunta = 0;
     numeroPuntos = 0;
     puntos.innerHTML = numeroPuntos;
+    continente = localStorage.getItem('continente');
     document.getElementById('acierto').style.display = 'none';
-    document.getElementById('siguientePregunta').style.display = 'none';
     document.getElementById('terminar').style.display = 'none';
+    datos[continente].sort(function () { return Math.random() - 0.5; });
+    for (let i = 0; i < datos[continente].length; i++) {
+        datos[continente][i].respuesta.sort(function () { return Math.random() - 0.5; });
+    };
     montarPregunta(numeroPregunta);
 };
 
-function desordenar(array) {
-    array = array.sort(function () { return Math.random() - 0.5; });
-    return array;
-};
-ordenPreguntas = desordenar(ordenPreguntas);
-ordenRespuestas = desordenar(ordenRespuestas);
 
 function montarPregunta(numeroPregunta) {
     document.getElementById('titulo').style.display = 'block';
@@ -53,21 +67,17 @@ function montarPregunta(numeroPregunta) {
     document.getElementById('mostrarNumeroPregunta').style.display = 'block';
     document.getElementById('acierto').style.display = 'none';
     document.getElementById('siguientePregunta').style.display = 'none';
-    preguntaActual = ordenPreguntas[numeroPregunta];
-    enunciado.innerHTML = listaPreguntas[preguntaActual][0];
+    enunciado.innerHTML = datos[continente][numeroPregunta].enunciado;
     mostrarNumeroPregunta.innerHTML = 'Pregunta ' + (numeroPregunta + 1);
-    for (var i = 0; i <= 3; i++) {
-        respuesta = listaPreguntas[ordenPreguntas[numeroPregunta]][ordenRespuestas[i]];
+    for (var i = 0; i < datos[continente][numeroPregunta].respuesta.length; i++) {
+        respuesta = datos[continente][numeroPregunta].respuesta[i];
         window['botonRespuesta' + (i + 1)].innerHTML = respuesta;
-    }
-    for (var i = 0; i <= 4; i++) {
-        document.getElementById('imagen' + i).src = '';
-    }
-    document.getElementById('imagen' + [ordenPreguntas[numeroPregunta]]).src = 'https://img.icons8.com/color/48/000000/' + listaPreguntas[ordenPreguntas[numeroPregunta]][5] + '-circular.png';
+    };
+    document.getElementById('imagen').src = 'https://img.icons8.com/color/48/000000/' + datos[continente][numeroPregunta].imagen + '-circular.png';
 };
 
 function comprobarRespuesta(respuesta) {
-    if (respuesta == listaPreguntas[ordenPreguntas[numeroPregunta]][6]) {
+    if (respuesta == datos[continente][numeroPregunta].correcta) {
         sonidoAcierto.play();
         document.getElementById('titulo').style.display = 'none';
         document.getElementById('enunciado').style.display = 'none';
@@ -76,7 +86,7 @@ function comprobarRespuesta(respuesta) {
         document.getElementById('mostrarNumeroPregunta').style.display = 'none';
         document.getElementById('acierto').style.display = 'block';
         document.getElementById('siguientePregunta').style.display = 'block';
-        document.getElementById('imagen' + ordenPreguntas[numeroPregunta]).src = '';
+        document.getElementById('imagen').src = '';
         numeroPuntos += 10;
         numeroPregunta += 1;
     }
@@ -89,7 +99,7 @@ function comprobarRespuesta(respuesta) {
 
 function siguientePregunta() {
     //Comprobar si quedan más preguntas
-    if (numeroPregunta < listaPreguntas.length) {
+    if (numeroPregunta < datos[continente].length) {
         document.getElementById('acierto').style.display = 'none'
         montarPregunta(numeroPregunta);
     }
@@ -98,50 +108,19 @@ function siguientePregunta() {
         document.getElementById('acierto').style.display = 'none'
         document.getElementById('terminar').style.display = 'block';
         localStorage.setItem('puntuacionFinal', numeroPuntos);
-    }
+        if (localStorage.getItem('puntuacionFinal') > localStorage.getItem('puntuacionMaxima')) {
+            localStorage.setItem('puntuacionMaxima', localStorage.getItem('puntuacionFinal'));
+        };
+    };
 };
 
-function mostrarPuntos() {
+function mostrarPuntuacionFinal() {
     puntuacionFinal.innerHTML = 'Tu puntuación ha sido de ' + localStorage.getItem('puntuacionFinal') + ' puntos';
 };
 
-
-// let ordenPreguntas = [0, 1, 2, 3, 4, 5]
-// function desordenar(){
-//     ordenPreguntas = ordenPreguntas.sort(function(){return Math.random() - 0.5;});
-//     return ordenPreguntas;
-// }
-// ordenPreguntas = desordenar();
-
-// console.log(ordenPreguntas);
-
-// console.log();
-// console.log(listaPreguntas[2]);
-// preguntaActual = listaPreguntas[ordenPreguntas][0];
-// console.log(listaPreguntas[ordenPreguntas[0]]);
-// preguntaActual = listaPreguntas[ordenPreguntas[0 + 1]];
-// console.log(preguntaActual);
-
-
-//FUNCIONAMIENTO PONIENDO ordenPreguntas EN CADA LÍNEA
-// function montarPregunta(numeroPregunta){
-//     document.getElementById('titulo').style.display = 'block';
-//     document.getElementById('enunciado').style.display = 'block';
-//     document.getElementById('grupoRespuestas').style.display = 'block';
-//     document.getElementById('mostrarNumeroPregunta').style.display = 'block';
-//     document.getElementById('acierto').style.display = 'none';
-//     document.getElementById('siguientePregunta').style.display = 'none';
-//     preguntaActual = listaPreguntas[ordenPreguntas[numeroPregunta]];
-//     enunciado.innerHTML = preguntaActual[0];
-//     mostrarNumeroPregunta.innerHTML = 'Pregunta ' + (numeroPregunta + 1);
-//     // console.log(numeroPregunta);
-//     console.log(listaPreguntas[ordenPreguntas[numeroPregunta]][i]);
-//     for (var i = 1; i<=4; i++){
-//         respuesta = listaPreguntas[ordenPreguntas[numeroPregunta]][i];
-//         window['botonRespuesta' + i].innerHTML = respuesta;
-//     }
-//     for (var i = 0; i<=4; i++){
-//         document.getElementById('imagen' + i).src = '';
-//     }
-//     document.getElementById('imagen' + numeroPregunta).src = 'https://img.icons8.com/color/48/000000/' + listaPreguntas[numeroPregunta][5] + '-circular.png';
-// };
+function mostrarPuntuacionMaxima() {
+    if (localStorage.getItem('puntuacionMaxima') == null) {
+        localStorage.setItem('puntuacionMaxima', 0);
+    }
+    puntuacionMaxima.innerHTML = 'Tu récord es de ' + localStorage.getItem('puntuacionMaxima') + ' puntos';
+}
